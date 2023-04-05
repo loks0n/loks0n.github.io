6a3ae0d1c595417c99b4c912014fb50799d95e0e
loks0n <22452787+loks0n@users.noreply.github.com>
2023-04-05 18:29:02 +0100

    fix: new url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment';
 
 export const SITE_TITLE = "Luke B. Silver's Blog";
 export const SITE_DESCRIPTION = 'Nothing much here.';
-export const SITE_URL = dev ? 'https://localhost:5173' : 'https://loks0n.github.io';
+export const SITE_URL = dev ? 'https://localhost:5173' : 'https://loks0n.dev';
