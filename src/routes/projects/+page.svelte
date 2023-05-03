7b3581d1984e47bee1930f82bac149f30c1b3ced
loks0n <22452787+loks0n@users.noreply.github.com>
2023-05-03 19:46:35 +0100

    feat: add island generator project

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -16,11 +16,19 @@
 				<a href="/projects/{project.slug}">{project.name}</a> - {project.description}
 			</li>
 		{/each}
+		<li>
+			<a href="https://island-generator-five.vercel.app/">Island Generator</a> - A procedural island
+			2D generator built using simplex noise. Features a Svelte based DOM renderer and a Canvas renderer.
+		</li>
 	</ul>
 </article>
 
 <style>
 	a {
 		font-weight: bold;
+	}
+
+	li {
+		margin-bottom: 0.5em;
 	}
 </style>
