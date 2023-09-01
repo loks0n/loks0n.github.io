535a5d20d3ffcb34aead19f25d6aa1528e130eac
loks0n <22452787+loks0n@users.noreply.github.com>
2023-09-01 14:17:27 +0100

    fix: ci & lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,12 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:svelte/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
