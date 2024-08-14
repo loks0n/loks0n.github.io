fdc37667f5665a029ac052a25c08f127b9e5422c
loks0n <22452787+loks0n@users.noreply.github.com>
2024-08-14 10:10:23 +0100

    chore: npm i

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// @ts-ignore
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 	kit: {
