02bb2d04860b6b051ac1bfa0f4efab4fb67155e9
loks0n <22452787+loks0n@users.noreply.github.com>
2025-12-24 14:44:56 +0000

    feat: migrate to shiki

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,10 +1,23 @@
-import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import { defineMDSveXConfig as defineConfig, escapeSvelte } from 'mdsvex';
 import relativeImages from 'mdsvex-relative-images';
+import { createHighlighter } from 'shiki';
+
+const theme = 'tokyo-night';
+const highlighter = await createHighlighter({
+	themes: [theme],
+	langs: ['javascript', 'typescript', 'svelte', 'html', 'css', 'json', 'bash', 'yaml']
+});
 
 const config = defineConfig({
 	extensions: ['.md', '.svx'],
 	smartypants: { dashes: 'oldschool' },
-	remarkPlugins: [relativeImages]
+	remarkPlugins: [relativeImages],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme }));
+			return `{@html \`${html}\`}`;
+		}
+	}
 });
 
 export default config;
